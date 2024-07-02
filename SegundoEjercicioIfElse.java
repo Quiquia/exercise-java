b7eeaf444f01c5723225d04d126ca4048c947400
Quiquia <vannebequi1994@gmail.com>
2024-07-02 22:31:34 +1000

    add exercise of if else

--- SegundoEjercicioIfElse.java
+++ SegundoEjercicioIfElse.java
@@ -6,8 +6,14 @@ public class SegundoEjercicioIfElse {
     public static void main(String[] args) {
         Scanner myscanner = new Scanner(System.in);
         // Llamar a los métodos para ejecutar la lógica
-       NumberPositivoNegativo(myscanner);
-       EstructuraControl(myscanner);
+
+        //NumberPositivoNegativo(myscanner);
+        //EstructuraControl(myscanner);
+        //Divisibility(myscanner);
+        //PasswordVerification(myscanner);
+        // DiscountCalculation(myscanner);
+        DeterminationOfAges(myscanner);
+
         
        // Cerrar el Scanner después de su uso
         myscanner.close();
@@ -17,7 +23,10 @@ public class SegundoEjercicioIfElse {
  
 
     public static void NumberPositivoNegativo(Scanner myScanner) {
-        int num = myScanner.nextInt();
+
+        System.out.println("Ingrese un número: ");
+        float num = myScanner.nextInt();
+        myScanner.nextLine();
     
         if (num > 0) {
             System.out.println("El número ingresado es positivo");
@@ -29,7 +38,9 @@ public class SegundoEjercicioIfElse {
     }
 
     public static void EstructuraControl(Scanner myScanner) {
-        int num = myScanner.nextInt();
+         System.out.println("Ingrese un número del 0 al 100: ");
+        float num = myScanner.nextInt();
+        myScanner.nextLine();
 
         if (num >= 0 && num <= 100) {
             if (num >= 90) {
@@ -48,7 +59,76 @@ public class SegundoEjercicioIfElse {
         }
     }
 
-   
+    public static void Divisibility(Scanner myScanner) {
+    
+        System.out.println("Ingrese un número: ");
+        float num = myScanner.nextInt();
+        myScanner.nextLine();
 
+
+        float divisibilityThree = num % 3;
+        float divisibilityFive = num % 5;
+
+        if (divisibilityFive == 0 && divisibilityThree == 0) {
+            System.out.println("Si es divisible por 3 y 5");   
+        } else {
+            System.out.println("NO es divisible por 3 y 5");   
+        }
+
+
+
+        
+    }
+   
+    public static void PasswordVerification(Scanner myScanner) {
+
+        String initPassword = "AABBCC";
+        System.out.println("Ingrese un Contraseña: ");
+        String password = myScanner.nextLine();
+
+        if (password.equals(initPassword)) {
+            System.out.println("Acceso Concedido");
+
+        } else {
+            System.out.println("Acceso Denegado");
+        }
+
+    }
+
+    public static void DiscountCalculation(Scanner myScanner) {
+
+        System.out.println("Ingrese el precio: ");
+        double price = myScanner.nextInt();
+        myScanner.nextLine();
+
+        // Verificar si el precio es igual o mayor a $100
+        if (price >= 100) {
+            // Verificar si el precio es igual o mayor a $100
+            double result = price - (price * 0.1);
+            // Mostrar el nuevo precio con descuento
+            System.out.println("El producto tiene un 10% de descuento, ahora le cuesta : $" + result);
+        } else {
+            // Mostrar el precio original sin descuento
+            System.out.println("El producto: $" + price);
+        }
+    }
+
+    public static void DeterminationOfAges(Scanner myScanner) {
+        System.out.println("Ingrese su edad: ");
+         float ages = myScanner.nextInt();
+         myScanner.nextLine();
+        
+         if (ages >= 18 && ages <= 64) {
+            System.out.println("Eres adulto");        
+        } else if (ages >= 65) {
+            System.out.println("Eres un adulto mayor");          
+        } else {
+            System.out.println("Eres menor de edad");          
+            
+         }
+
+
+        
+    }
 }
 
