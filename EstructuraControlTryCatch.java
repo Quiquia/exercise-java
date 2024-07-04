b7cd045f5dabd6a0d35e80d99f30f9887abf8226
Quiquia <vannebequi1994@gmail.com>
2024-07-04 11:53:36 +1000

    add excercise trycatch

--- EstructuraControlTryCatch.java
+++ EstructuraControlTryCatch.java
@@ -7,7 +7,10 @@ public class EstructuraControlTryCatch {
         Scanner myScanner = new Scanner(System.in);
 
         // Llamar a los métodos para ejecutar la lógica
-        Division(myScanner);
+        // Division(myScanner);
+        // Division2(myScanner);
+        // Subtraction(myScanner);
+        ConversionfromStringtoInteger(myScanner);
 
         myScanner.close();
 
@@ -15,33 +18,76 @@ public class EstructuraControlTryCatch {
     
     public static void Division(Scanner myScanner) {
         System.out.println("Ingrese dos número: ");
-        int num1 = 0;
-        int num2 = 0;
+        int num1;
+        int num2;
 
         try {
-          num1 = myScanner.nextInt();
-          num2 = myScanner.nextInt();
-          int result = num1 / num2;
-          System.out.println("El resultado es: " + result);
+            num1 = myScanner.nextInt();
+            num2 = myScanner.nextInt();
+            double result = num1 / num2;
+            System.out.println("El resultado es: " + result);
 
-            
         } catch (ArithmeticException e) {
             // Todo: handle exception
             System.out.println("Error: No es posible dividir por 0.");
-        }
-        catch (InputMismatchException e) {
+        } catch (InputMismatchException e) {
             // Todo: handle exception
-            System.out.println("Error: Se detectó un valor inválido ingresado"
-);
+            System.out.println("Error: Se detectó un valor inválido ingresado");
+        }
+    }
+    
+    public static void Division2(Scanner myScanner) {
+        try {
+    System.out.print("Ingrese un divisor: ");
+    int numero = myScanner.nextInt();// Posible entrada inválida
+    // String palabra = "hola";
+    float resultado = 10 / numero ;// Posible división por cero
+    System.out.println("El resultado es: " + resultado);
+    } catch (ArithmeticException e) {
+    System.out.println("Error: División por cero.");
+    } catch (InputMismatchException e) {
+        System.out.println("Error: Se detectó un valor inválido ingresado por teclado.");
+    } catch (Exception e) {
+    System.out.println("Error: Ups!");  
+    }
+    }
+    
+    public static void Subtraction(Scanner myScanner) {
+        System.out.println("Ingrese dos número: ");
+        int num1;
+        int num2;
 
+        float result = 0;
+
+        try {
+            num1 = myScanner.nextInt();
+            num2 = myScanner.nextInt();
+            result = num1 - num2;
+            System.out.println("El resultado es: " + result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: Ups!: " + e.getMessage());
         }
 
+    }
 
+    public static void ConversionfromStringtoInteger(Scanner myScanner) {
+         System.out.println("Ingrese un número: ");
+         String stringNumber;
+          
+         try {
+         stringNumber = myScanner.nextLine();
+        float number = Integer.parseInt(stringNumber);
+         System.out.println("El número es: " + number);
+            
+         } catch (NumberFormatException e) {
+             // Todo: handle exception
+         System.out.println("El caracter ingresado no se puede convertir en número");
 
-
+            
+         }
 
         
     }
-    
-
 }
